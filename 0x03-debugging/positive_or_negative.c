a5717c54967663c9c1bfa2cbf2d4b748b86d82da
Agboola Sarah <agboolasarah2004@gmail.com>
2023-03-23 12:20:10 -0700

    main 1

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * positive_or_negative - assign numbers either positve or negative
+ * check for positive and negative number
+ * @i: integer
+ * if the number is greater than 0: is positive
+ * else if the number is less than 0: is negative
+ *
+ * Return: always 0
+ */
+
+void positive_or_negative(int i);
+(
+if(i < 0)
+if(i < 0)
+printf("%d is negative\n", i);
+else if(i > 0)
+printf("%d is positive\n", i);
+else
+printf("%d is zero\n", i);
+)

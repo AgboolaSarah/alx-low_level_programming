837852807a97c44e5cc95d0551122c964454889f
Agboola Sarah <agboolasarah2004@gmail.com>
2023-03-28 05:33:18 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+# ifndef MAIN_H
+# define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset to _98(int 'n);
+void swap_int(int 'a, int 'b);
+int _strlen(char 'a);
+void _putchar(char 'strlen);
+void print_rev(char 'a);
+void rev_string(char 'a);
+void puts2(char 'str);
+void puts_half(char 'str);
+void print_array(int 'a, int n);
+char '_strcpy(char 'dest, char 'arc);
+#endif
